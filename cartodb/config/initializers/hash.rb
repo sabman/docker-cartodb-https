b304ca068b6904e527deea48b003f3875eed3e75
Guillaume Larocque <glaroc@gmail.com>
2019-05-15 15:06:19 +0000

    First https version fully working

--- cartodb/config/initializers/hash.rb
+++
@@ -1,7 +0,0 @@
-class Hash
-  
-  def convert_nulls
-    self.inject({}){|h,(k,v)| h[k] = (v == "null" ? nil : v); h }
-  end
-  
-end
--- cartodb/config/initializers/statsd.rb
+++
@@ -1,8 +0,0 @@
-require 'statsd'
-
-begin
-  Statsd.host = Cartodb.config[:graphite]['host']
-  Statsd.port = Cartodb.config[:graphite]['port']
-rescue => e
-  Rails.logger.info "Ignoring statsd, because there were a error loading the config"
-end
