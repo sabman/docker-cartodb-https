68e71fbac7e7f7f31b5c709e4be5a4c8eed9b1e4
Anthony Esper <anthony.m.esper@gmail.com>
2019-07-05 13:30:01 -0400

    Update geocoder.sh
    additional entries for geocoder user and org

--- geocoder.sh
+++ geocoder.sh
@@ -28,13 +28,13 @@ psql -U postgres -d $GEOCODER_DB -c "BEGIN;GRANT EXECUTE ON ALL FUNCTIONS IN SCH
 # dev user
 USER_DB=`echo "SELECT database_name FROM users WHERE username=$CARTO_USER_SUBDOMAIN" | psql -U postgres -t carto_db_production`
 echo "CREATE EXTENSION cdb_dataservices_client;" | psql -U postgres $USER_DB
-echo "SELECT CDB_Conf_SetConf('user_config', '{"'"is_organization"'": false, "'"entity_name"'": "'"prod"'"}');" | psql -U postgres $USER_DB
+echo "SELECT CDB_Conf_SetConf('user_config', '{"'"is_organization"'": false, "'"entity_name"'": "'"$CARTO_USER_SUBDOMAIN"'"}');" | psql -U postgres $USER_DB
 echo -e "SELECT CDB_Conf_SetConf('geocoder_server_config', '{ \"connection_str\": \"host=localhost port=5432 dbname=${GEOCODER_DB# } user=postgres\"}');" | psql -U postgres $USER_DB
-bundle exec  rake cartodb:services:set_user_quota['prod',geocoding,100000]
+bundle exec  rake cartodb:services:set_user_quota['$CARTO_USER_SUBDOMAIN',geocoding,100000]
 
 # example organization
 ORGANIZATION_DB=`echo "SELECT database_name FROM users WHERE username=$CARTO_ORG_USERNAME" | psql -A -U postgres -t carto_db_production`
 echo "CREATE EXTENSION cdb_dataservices_client;" | psql -U postgres $ORGANIZATION_DB
-echo "SELECT CDB_Conf_SetConf('user_config', '{"'"is_organization"'": true, "'"entity_name"'": "'"example"'"}');" | psql -U postgres $ORGANIZATION_DB
+echo "SELECT CDB_Conf_SetConf('user_config', '{"'"is_organization"'": true, "'"entity_name"'": "'"$CARTO_ORG_USERNAME"'"}');" | psql -U postgres $ORGANIZATION_DB
 echo -e "SELECT CDB_Conf_SetConf('geocoder_server_config', '{ \"connection_str\": \"host=localhost port=5432 dbname=${GEOCODER_DB# } user=postgres\"}');" | psql -U postgres $ORGANIZATION_DB
-bundle exec  rake cartodb:services:set_org_quota['example',geocoding,100000]
+bundle exec  rake cartodb:services:set_org_quota['$CARTO_ORG_USERNAME',geocoding,100000]
