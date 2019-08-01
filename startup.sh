77ffcccb470179c541e3a9754e7529fdd7e0188b
anthonyesper <51342402+anthonyesper@users.noreply.github.com>
2019-08-01 14:11:39 -0400

    Comment out Script Tables causing Issues

--- startup.sh
+++ startup.sh
@@ -26,7 +26,7 @@ node app.js production &
 cd /cartodb
 bundle exec script/restore_redis
 bundle exec script/resque > resque.log 2>&1 &
-script/sync_tables_trigger.sh &
+#script/sync_tables_trigger.sh &
 
 # Recreate api keys in db and redis, so sql api is authenticated
 echo 'delete from api_keys' | psql -U postgres -t carto_db_production
